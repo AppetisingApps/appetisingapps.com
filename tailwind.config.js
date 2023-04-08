b62aa40ad186ff55f189456ea88b4de6a2f7b686
Stephen Samra <stephen@stephensamra.com>
2023-04-08 15:51:15 +0100

    remove vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './src/**/*.tsx',
-    './src/**/*.ts',
-    './index.html',
-    './public/**/*.html',
+  content: [
+    './public/index.html',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
